9b7048c95024287d35aa0a146c952bae6b046899
Maltthael <macrodrigues01@hotmail.com>
2026-03-04 14:22:33 -0300

    Customizing admin page

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,4 +3,10 @@ from galeria.models import Fotografia
 
 # Register your models here.
 
-admin.site.register(Fotografia)
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+admin.site.register(Fotografia, ListandoFotografias)
