be3af4056457b4d513b586559d724e33778492c5
Maltthael <macrodrigues01@hotmail.com>
2026-03-09 12:57:46 -0300

    Creating login and register pages

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galeria.apps.GaleriaConfig',
-
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #Media
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
